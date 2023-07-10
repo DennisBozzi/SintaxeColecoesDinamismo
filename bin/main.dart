1a972640b54e8fc47ca735e5d89d7d02c527c363
Dennis Bozzi <dennisbozzii@gmail.com>
2023-07-10 17:05:29 -0300

    Utilizando enum e switch case

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,41 @@
-void main() {}
+void main() {
+  escolherMeioTransporteEnum(Transporte.trem);
+}
+
+//Comparação sem Enum e sem Switch
+void escolherMeioTransporte(int locomocao) {
+  if (locomocao == 0) {
+    print("Vou de CARRO para a aventura!");
+  } else if (locomocao == 1) {
+    print("Vou de BIKE para a aventura!");
+  } else {
+    print("Vou para a aventura!");
+  }
+}
+
+enum Transporte { carro, bike, andando, skate, aviao, patins, trem }
+
+//Comparação com Enum e Switch
+void escolherMeioTransporteEnum(Transporte locomocao) {
+  switch (locomocao) {
+    case Transporte.carro:
+      print('Vou de CARRO para a aventura!');
+      break;
+
+    case Transporte.bike:
+      print('Vou de BIKE para a aventura!');
+      break;
+
+    case Transporte.andando:
+      print('Vou ANDANDO para a aventura!');
+      break;
+
+    case Transporte.skate:
+      print('Vou de SKATE para a aventura!');
+      break;
+
+    default:
+      print('Vou para a aventura!');
+      break;
+  }
+}
